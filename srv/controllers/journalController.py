bef5a14cb97288db386153a67c97437676f7dad1
Eryk Dudkiewicz <128227580+1iekki@users.noreply.github.com>
2025-05-16 16:43:19 +0200

    get fix

--- srv/controllers/journalController.py
+++ srv/controllers/journalController.py
@@ -9,9 +9,9 @@ from fastapi import status
 @app.get("/journals/{journal_id}", response_model=Journal)
 def read_user(journal_id: int, session: SessionDep):
     journal = session.get(Journal, journal_id)
-    if not Journal:
+    if not journal:
         raise HTTPException(status_code=404, detail="Journal not found")
-    return Journal
+    return journal
 
 
 @app.post("/journals/", response_model=Journal, status_code=status.HTTP_201_CREATED)

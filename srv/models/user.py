5a12ffc17937e887a9ec4b4bba1a637d3e2c56b0
life8853 <life88853@gmail.com>
2025-05-17 01:09:48 +0200

    New user

--- srv/models/user.py
+++ srv/models/user.py
@@ -1,7 +1,21 @@
+from typing import List
+from pydantic import BaseModel
 from sqlmodel import Field, SQLModel, Relationship
+
+from .form import Form
 
 class User(SQLModel, table=True):
     id: int | None = Field(default=None, primary_key=True)
     username: str = Field(index=True)
     password: str = Field(index=True)
     streak: int = Field()
+
+    forms: List["Form"] = Relationship(back_populates="user")
+
+class UserRead(BaseModel):
+    id: int
+    username: str
+    password: str
+    streak: int
+
+    forms: List["Form"] = []
